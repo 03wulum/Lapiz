ae5e0a3da89cc02051cdf87dac2c40fa44692904
Obinna Ewulum <obinnaewulum@gmail.com>
2022-01-26 23:17:28 -0500

    initial commit

---
+++ src/components/qoute/qCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+  <div class="container">
+    <h4><b>Jane Doe</b></h4> 
+    <p>sdasadsasdasadsasdasadsasdasadsa</p> 
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
