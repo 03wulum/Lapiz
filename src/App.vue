04b68ad5276eb5c2f94b03be4a5e9217b8f8e50a
Obinna Ewulum <obinnaewulum@gmail.com>
2022-01-27 00:17:07 -0500

    Bring in router.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
-<div>
-  <Home/>
-  <NavBar/>
-</div>
- 
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <div>Lapiz</div>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Profile</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <!-- <v-main>
+      <HelloWorld/>
+    </v-main> -->
+  </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import NavBar from './components/Nav/NavBar.vue';
-import Home from './views/Home.vue';
 
 export default Vue.extend({
   name: 'App',
 
   components: {
-    Home,
-    NavBar,
   },
 
   data: () => ({
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import Vuetify from 'vuetify';
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
 
 Vue.use(Vuetify);
