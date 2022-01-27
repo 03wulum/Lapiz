97191991ca97acee0f8eaae3d119705f94af7200
Obinna Ewulum <obinnaewulum@gmail.com>
2022-01-27 00:10:12 -0500

    Setup vuetify. nav appears but is wonky.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
+    <hello-world/>
+      <nav-bar/>
       <h1>
          Home view
       </h1>
   </div>
 </template>
 
-<script>
-export default {
-
-}
+<script lang="ts">
+import Vue from 'vue'
+import NavBar from '../components/Nav/NavBar.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+// import navigation from '../components/Nav/NavBar.vue'
+export default Vue.extend({
+    components: {
+    NavBar,
+    HelloWorld,
+  },
+})
 </script>
 
-<style>
-
-</style>
