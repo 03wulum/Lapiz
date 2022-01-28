d88c566176af6d96baff229aff78924a978846d6
Obinna Ewulum <obinnaewulum@gmail.com>
2022-01-28 01:05:52 -0500

    Shift Profile picture for parent qoute, change main qoute's text style. Added comment icon

--- src/components/qoute/qCard.vue
+++ src/components/qoute/qCard.vue
@@ -6,15 +6,22 @@
     dark
     max-width="400"
   >
-    <v-card-title>
+    <!-- <v-card-title>
       <span class="text-h6 font-weight-light">Lapis</span>
-    </v-card-title>
+              <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            alt=""
+            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          ></v-img>
+        </v-list-item-avatar>
 
-    <v-card-text class="text-h5 font-weight-bold">
-      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
-    </v-card-text>
-
-    <v-card-actions>
+        <v-list-item-content>
+   
+        </v-list-item-content>
+       <v-list-item-title>Evan You</v-list-item-title>
+    </v-card-title> -->
+      <v-card-actions>
       <v-list-item class="grow">
         <v-list-item-avatar color="grey darken-3">
           <v-img
@@ -44,10 +51,55 @@
         </v-row>
       </v-list-item>
     </v-card-actions>
-    <div class="thread" >
-      <div style="width: 280px">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
+    <!-- <v-card-text class="text-h5 white--text font-weight-bold">
+      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+    </v-card-text> -->
+    
+    <div class="parentQ" >
+      <div class="qReply" style="width: 280px">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </div>
+
+    <v-card-actions>
+      <v-list-item class="grow">
+
+        <v-row
+          align="center"
+          justify="end"
+        >
+          <v-icon class="mr-1">
+            mdi-heart
+          </v-icon>
+          <span class="subheading mr-2">256</span>
+
+          <v-icon class="mr-1">
+            mdi-share-variant
+          </v-icon>
+
+          <span class="subheading">45</span>
+                    <v-icon class="mr-1">mdi-comment</v-icon>
+                              <span class="subheading">45</span>
+        </v-row>
+      </v-list-item>
+    </v-card-actions>
+    <div v-if="isQreplyToggled">
+       <v-col
+        cols="12"
+        md="12"
+      >
+        <v-textarea
+          solo
+          name="input-7-4"
+          label="Solo textarea"
+        ></v-textarea>
+      </v-col>   
+    </div>
+
+    <div class="thread" >
+      <div class="qReply" style="width: 280px">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
     </div>
     <v-card-actions>
@@ -84,17 +136,39 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 </div>
 </template>
 
-<script>
-export default {
 
-}
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'HelloWorld',
+
+    props:  {
+      isQreplyToggled: {
+        required: true,
+        type: Boolean,
+        default: false
+      }
+    },
+
+    data: () => ({
+    }),
+  })
 </script>
 
 <style>
   .thread {
-    border-left: 6px solid black;
+    border-left: 4px solid black;
     position:relative;
     margin-left: 40px;
     overflow:hidden
+  }
+  .parentQ {
+    position:relative;
+    margin-left: 40px;
+    overflow:hidden
+  }
+  .qReply {
+    margin-left: 15px;
   }
 </style>
