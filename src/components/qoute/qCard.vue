86f3710a53bc20ab207f487527a7b268502ce051
Obinna Ewulum <obinnaewulum@gmail.com>
2022-01-28 01:28:10 -0500

    Align text within the card. remove excess icons and v items.

--- src/components/qoute/qCard.vue
+++ src/components/qoute/qCard.vue
@@ -6,21 +6,6 @@
     dark
     max-width="400"
   >
-    <!-- <v-card-title>
-      <span class="text-h6 font-weight-light">Lapis</span>
-              <v-list-item-avatar color="grey darken-3">
-          <v-img
-            class="elevation-6"
-            alt=""
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-          ></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-   
-        </v-list-item-content>
-       <v-list-item-title>Evan You</v-list-item-title>
-    </v-card-title> -->
       <v-card-actions>
       <v-list-item class="grow">
         <v-list-item-avatar color="grey darken-3">
@@ -31,24 +16,6 @@
           ></v-img>
         </v-list-item-avatar>
 
-        <v-list-item-content>
-          <v-list-item-title>Evan You</v-list-item-title>
-        </v-list-item-content>
-
-        <v-row
-          align="center"
-          justify="end"
-        >
-          <v-icon class="mr-1">
-            mdi-heart
-          </v-icon>
-          <span class="subheading mr-2">256</span>
-
-          <v-icon class="mr-1">
-            mdi-share-variant
-          </v-icon>
-          <span class="subheading">45</span>
-        </v-row>
       </v-list-item>
     </v-card-actions>
 
@@ -57,7 +24,7 @@
     </v-card-text> -->
     
     <div class="parentQ" >
-      <div class="qReply" style="width: 280px">
+      <div class="qReply" style="width: 280px"> <v-list-item-title>Evan You</v-list-item-title>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
     </div>
@@ -161,14 +128,19 @@
     border-left: 4px solid black;
     position:relative;
     margin-left: 40px;
-    overflow:hidden
+    overflow: hidden;
+    text-align: left
   }
   .parentQ {
     position:relative;
     margin-left: 40px;
-    overflow:hidden
+    overflow: hidden;
+    text-align: left;
   }
   .qReply {
-    margin-left: 15px;
+    margin-left: 15px;   
+    word-break: break-word;
+    white-space: normal;
+    text-align: left;
   }
 </style>
