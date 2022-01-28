3fb8f2fae9be300a578e1729aeac0a37216fb1bc
Obinna Ewulum <obinnaewulum@gmail.com>
2022-01-28 00:24:01 -0500

    Create UI thread for comments or replies

--- src/components/qoute/qCard.vue
+++ src/components/qoute/qCard.vue
@@ -7,12 +7,6 @@
     max-width="400"
   >
     <v-card-title>
-      <v-icon
-        large
-        left
-      >
-        mdi-twitter
-      </v-icon>
       <span class="text-h6 font-weight-light">Lapis</span>
     </v-card-title>
 
@@ -42,7 +36,43 @@
             mdi-heart
           </v-icon>
           <span class="subheading mr-2">256</span>
-          <span class="mr-1">·</span>
+
+          <v-icon class="mr-1">
+            mdi-share-variant
+          </v-icon>
+          <span class="subheading">45</span>
+        </v-row>
+      </v-list-item>
+    </v-card-actions>
+    <div class="thread" >
+      <div style="width: 280px">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      </div>
+    </div>
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            alt=""
+            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Evan You</v-list-item-title>
+        </v-list-item-content>
+
+        <v-row
+          align="center"
+          justify="end"
+        >
+          <v-icon class="mr-1">
+            mdi-heart
+          </v-icon>
+          <span class="subheading mr-2">256</span>
+
           <v-icon class="mr-1">
             mdi-share-variant
           </v-icon>
@@ -61,5 +91,10 @@ export default {
 </script>
 
 <style>
-
+  .thread {
+    border-left: 6px solid black;
+    position:relative;
+    margin-left: 40px;
+    overflow:hidden
+  }
 </style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="profile">
+  <div class="container">
       <h1>Profile</h1>
+    <q-card/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import qCard from '../components/qoute/qCard.vue'
 export default {
   name: 'Profile',
   components: {
+      qCard
   }
 }
 </script>
